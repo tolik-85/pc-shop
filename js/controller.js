407cbd9c8fa8ff02cc14977243f4fb3d0a619164
tolik-85 <tolian8576@gmail.com>
2024-04-06 12:18:00 +0300

    change view and controller to object view and controller

--- js/controller.js
+++ js/controller.js
@@ -1,31 +1,37 @@
-async function handleAvailibleProducts() {
-  const products = await model.getProducts()
-  model.setAvalibleProducts(products)
-  model.avalibleProducts.forEach(el => {
-    const URL = 'https://web-app.click/photos/products/computers/'
-    const picture = el.photos.files[0]
-    const purpose = el.purpose
-    const caption = el.caption
-    const price = el.price
-    const image = `${URL}${picture}`
-    renderContainerProducts(purpose, caption, price, image)
-  })
-}
+const controller = {
+  async handleAvailibleProducts() {
+    const products = await model.getProducts()
+    model.setAvalibleProducts(products)
+    model.avalibleProducts.forEach(el => {
+      const URL = 'https://web-app.click/photos/products/computers/'
+      const picture = el.photos.files[0]
+      const purpose = el.purpose
+      const caption = el.caption
+      const price = el.price
+      const image = `${URL}${picture}`
+      view.renderContainerProducts(purpose, caption, price, image)
+    })
+  },
 
-function renderWrapFilter() {
-  for (const key in model.filterModel) {
-    const filterCategory = generateFilterCategory(key)
-    const elWrapFilter = document.querySelector('.wrap-filter')
-    elWrapFilter.appendChild(filterCategory)
-    for (const name in model.filterModel[key]) {
-      const filterSubCategory = generateFilterSubCategory(name)
-      const filterGroupWrappers = generateFilterGroupWrappers()
-      filterCategory.appendChild(filterSubCategory)
-      filterSubCategory.appendChild(filterGroupWrappers)
-      for (let value of model.filterModel[key][name]) {
-        const chBoxes = generateFilterWrapCheckBox(value)
-        filterGroupWrappers.appendChild(chBoxes)
+  handleFilter() {
+    model.filterForCategories()
+  },
+
+  renderWrapFilter() {
+    for (const key in model.filterModel) {
+      const elFilterCategory = view.generateFilterCategory(key)
+      const elWrapFilter = document.querySelector('.wrap-filter')
+      elWrapFilter.appendChild(elFilterCategory)
+      for (const name in model.filterModel[key]) {
+        const elFilterSubCategory = view.generateFilterSubCategory(name)
+        const elFilterGroupWrappers = view.generateFilterGroupWrappers()
+        elFilterCategory.appendChild(elFilterSubCategory)
+        elFilterSubCategory.appendChild(elFilterGroupWrappers)
+        for (let value of model.filterModel[key][name]) {
+          const chBoxes = view.generateFilterWrapCheckBox(value)
+          elFilterGroupWrappers.appendChild(chBoxes)
+        }
       }
     }
-  }
+  },
 }
--- js/view.js
+++ js/view.js
@@ -1,122 +1,120 @@
-document.addEventListener('DOMContentLoaded', onLoaded)
+const view = {
+  async onLoaded() {
+    await controller.handleAvailibleProducts()
+    controller.handleFilter()
+    controller.renderWrapFilter()
+  },
+  renderContainerProducts(purpose, caption, price, image) {
+    const elContainerProducts = document.querySelector('.container-products')
+    const ElProduct = this.generateProduct(purpose, caption, price, image)
+    elContainerProducts.appendChild(ElProduct)
+  },
 
-async function onLoaded() {
-  await handleAvailibleProducts()
-  model.filterForCategories()
-  renderWrapFilter()
+  generateFilterCategory(filterCategory) {
+    const elWrapProps = document.createElement('div')
+    const elH3 = document.createElement('h3')
+    elWrapProps.classList.add('filter-category')
+    elH3.innerHTML = `${filterCategory}`
+    elWrapProps.appendChild(elH3)
+    return elWrapProps
+  },
+  generateFilterSubCategory(filterSubCategoty) {
+    const elWrapSubcategory = document.createElement('div')
+    const elH4 = document.createElement('h4')
+    elWrapSubcategory.classList.add('filter-subcategoru')
+    elH4.innerHTML = `${filterSubCategoty}`
+    elWrapSubcategory.appendChild(elH4)
+    return elWrapSubcategory
+  },
+
+  generateFilterGroupWrappers() {
+    const elGroupWrappers = document.createElement('div')
+    elGroupWrappers.classList.add('group-wrappers')
+    return elGroupWrappers
+  },
+
+  generateFilterWrapCheckBox(brand) {
+    const elWrap = document.createElement('div')
+    const elWrapCheckBox = document.createElement('div')
+    const elInputCheckbBox = document.createElement('input')
+
+    elWrap.classList.add('wrap')
+    elWrapCheckBox.classList.add('wrap-checkbox')
+    elInputCheckbBox.setAttribute('type', 'checkbox')
+    elInputCheckbBox.setAttribute('name', `${brand}`)
+    elInputCheckbBox.setAttribute('id', `${brand}`)
+    elInputCheckbBox.setAttribute('value', `${brand}_01`)
+    const elLabelForCheckbox = document.createElement('label')
+    elLabelForCheckbox.setAttribute('for', `${brand}`)
+    elLabelForCheckbox.innerHTML = `${brand}`
+
+    elWrapCheckBox.appendChild(elInputCheckbBox)
+    elWrapCheckBox.appendChild(elLabelForCheckbox)
+
+    return elWrapCheckBox
+  },
+  generateProduct(purpose, caption, price, image) {
+    const wrapProduct = document.createElement('div')
+    const wrapImg = document.createElement('div')
+    const img = document.createElement('img')
+    const divForH3 = document.createElement('div')
+    const h3 = document.createElement('h3')
+    const divForLabels = document.createElement('div')
+    const laberForBrand = document.createElement('label')
+    const laberForpurpose = document.createElement('label')
+    const laberForRam = document.createElement('label')
+    const laberForGpu = document.createElement('label')
+    const laberForSsd = document.createElement('label')
+    const divForPrice = document.createElement('div')
+    const parForPrice = document.createElement('p')
+    const row = document.createElement('div')
+    const divForCart = document.createElement('div')
+    const cartBtn = document.createElement('button')
+    const divForFavorites = document.createElement('div')
+    const favoritesBtn = document.createElement('button')
+    const divForCompare = document.createElement('div')
+    const compareBtn = document.createElement('button')
+
+    wrapProduct.classList.add('wrap-product')
+    wrapImg.classList.add('wrap-img')
+    img.setAttribute('src', `${image}`)
+    img.setAttribute('alt', 'product')
+    h3.innerHTML = `${caption}`
+    laberForpurpose.setAttribute('for', 'purpose')
+    laberForpurpose.innerHTML = `${purpose}`
+    laberForBrand.setAttribute('for', 'brand_01')
+    laberForBrand.innerHTML = 'ASUS'
+    laberForRam.setAttribute('for', 'ram_03')
+    laberForRam.innerHTML = '16 Gb'
+    laberForGpu.setAttribute('for', 'gpu_03')
+    laberForGpu.innerHTML = 'Radeon RX'
+    laberForSsd.setAttribute('for', 'ssd_03')
+    laberForSsd.innerHTML = '1 Tb'
+    parForPrice.innerHTML = `<b>${price}</b> грн`
+    row.classList.add('row')
+
+    wrapImg.appendChild(img)
+    divForH3.appendChild(h3)
+    divForLabels.appendChild(laberForBrand)
+    divForLabels.appendChild(laberForGpu)
+    divForLabels.appendChild(laberForRam)
+    divForLabels.appendChild(laberForSsd)
+    divForPrice.appendChild(parForPrice)
+    divForCart.appendChild(cartBtn)
+    divForFavorites.appendChild(favoritesBtn)
+    divForCompare.appendChild(compareBtn)
+    row.appendChild(divForCart)
+    row.appendChild(divForFavorites)
+    row.appendChild(divForCompare)
+
+    wrapProduct.appendChild(wrapImg)
+    wrapProduct.appendChild(divForH3)
+    wrapProduct.appendChild(divForLabels)
+    wrapProduct.appendChild(divForPrice)
+    wrapProduct.appendChild(row)
+
+    return wrapProduct
+  },
 }
 
-const elContainerProducts = document.querySelector('.container-products')
-
-function renderContainerProducts(purpose, caption, price, image) {
-  const ElProduct = generateProduct(purpose, caption, price, image)
-  elContainerProducts.appendChild(ElProduct)
-}
-
-function generateFilterCategory(filterCategory) {
-  const elWrapProps = document.createElement('div')
-  const elH3 = document.createElement('h3')
-  elWrapProps.classList.add('filter-category')
-  elH3.innerHTML = `${filterCategory}`
-  elWrapProps.appendChild(elH3)
-  return elWrapProps
-}
-
-function generateFilterSubCategory(filterSubCategoty) {
-  const elWrapSubcategory = document.createElement('div')
-  const elH4 = document.createElement('h4')
-  elWrapSubcategory.classList.add('filter-subcategoru')
-  elH4.innerHTML = `${filterSubCategoty}`
-  elWrapSubcategory.appendChild(elH4)
-  return elWrapSubcategory
-}
-
-function generateFilterGroupWrappers() {
-  const elGroupWrappers = document.createElement('div')
-  elGroupWrappers.classList.add('group-wrappers')
-  return elGroupWrappers
-}
-
-function generateFilterWrapCheckBox(brand) {
-  const elWrap = document.createElement('div')
-  const elWrapCheckBox = document.createElement('div')
-  const elInputCheckbBox = document.createElement('input')
-
-  elWrap.classList.add('wrap')
-  elWrapCheckBox.classList.add('wrap-checkbox')
-  elInputCheckbBox.setAttribute('type', 'checkbox')
-  elInputCheckbBox.setAttribute('name', `${brand}`)
-  elInputCheckbBox.setAttribute('id', `${brand}`)
-  elInputCheckbBox.setAttribute('value', `${brand}_01`)
-  const elLabelForCheckbox = document.createElement('label')
-  elLabelForCheckbox.setAttribute('for', `${brand}`)
-  elLabelForCheckbox.innerHTML = `${brand}`
-
-  elWrapCheckBox.appendChild(elInputCheckbBox)
-  elWrapCheckBox.appendChild(elLabelForCheckbox)
-
-  return elWrapCheckBox
-}
-
-function generateProduct(purpose, caption, price, image) {
-  const wrapProduct = document.createElement('div')
-  const wrapImg = document.createElement('div')
-  const img = document.createElement('img')
-  const divForH3 = document.createElement('div')
-  const h3 = document.createElement('h3')
-  const divForLabels = document.createElement('div')
-  const laberForBrand = document.createElement('label')
-  const laberForpurpose = document.createElement('label')
-  const laberForRam = document.createElement('label')
-  const laberForGpu = document.createElement('label')
-  const laberForSsd = document.createElement('label')
-  const divForPrice = document.createElement('div')
-  const parForPrice = document.createElement('p')
-  const row = document.createElement('div')
-  const divForCart = document.createElement('div')
-  const cartBtn = document.createElement('button')
-  const divForFavorites = document.createElement('div')
-  const favoritesBtn = document.createElement('button')
-  const divForCompare = document.createElement('div')
-  const compareBtn = document.createElement('button')
-
-  wrapProduct.classList.add('wrap-product')
-  wrapImg.classList.add('wrap-img')
-  img.setAttribute('src', `${image}`)
-  img.setAttribute('alt', 'product')
-  h3.innerHTML = `${caption}`
-  laberForpurpose.setAttribute('for', 'purpose')
-  laberForpurpose.innerHTML = `${purpose}`
-  laberForBrand.setAttribute('for', 'brand_01')
-  laberForBrand.innerHTML = 'ASUS'
-  laberForRam.setAttribute('for', 'ram_03')
-  laberForRam.innerHTML = '16 Gb'
-  laberForGpu.setAttribute('for', 'gpu_03')
-  laberForGpu.innerHTML = 'Radeon RX'
-  laberForSsd.setAttribute('for', 'ssd_03')
-  laberForSsd.innerHTML = '1 Tb'
-  parForPrice.innerHTML = `<b>${price}</b> грн`
-  row.classList.add('row')
-
-  wrapImg.appendChild(img)
-  divForH3.appendChild(h3)
-  divForLabels.appendChild(laberForBrand)
-  divForLabels.appendChild(laberForGpu)
-  divForLabels.appendChild(laberForRam)
-  divForLabels.appendChild(laberForSsd)
-  divForPrice.appendChild(parForPrice)
-  divForCart.appendChild(cartBtn)
-  divForFavorites.appendChild(favoritesBtn)
-  divForCompare.appendChild(compareBtn)
-  row.appendChild(divForCart)
-  row.appendChild(divForFavorites)
-  row.appendChild(divForCompare)
-
-  wrapProduct.appendChild(wrapImg)
-  wrapProduct.appendChild(divForH3)
-  wrapProduct.appendChild(divForLabels)
-  wrapProduct.appendChild(divForPrice)
-  wrapProduct.appendChild(row)
-
-  return wrapProduct
-}
+document.addEventListener('DOMContentLoaded', view.onLoaded)
