71218bdb7f4f51857c247b578d9ff8c0c4faa240
YouROCK <113207809+yourock1988@users.noreply.github.com>
2024-05-07 16:11:00 +0300

    remove card_api.js

--- js/api.js
+++ js/api.js
@@ -1,21 +1,42 @@
-// const API_URL = 'http://35.225.111.193:8181/api/v3/products/computers'
-const API_URL = 'https://web-app.click/pc-shop/api/v0/products/'
+const API_URL = 'https://web-app.click/pc-shop/api/v0/'
 
+const API_URL_PRODUCTS = API_URL + 'products/'
 const API_URL_COURSE =
   'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json'
-// const API_URL_COURSE =
-//   'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5'
+
+// const API_SIMILAR_PRODUCTS = API_URL + `products/${id}/similar/`
 
 const api = {
   async loadProducts() {
     const resp = await fetch(`${API_URL}`)
-
-    return (await resp.json()).payload
+    const json = await resp.json()
+    return json.payload
   },
 
   async loadCourse() {
-    // console.log('loadCourse')
     const resp = await fetch(`${API_URL_COURSE}`)
+    const json = await resp.json()
+    const currency = json.find(o => o.cc === 'USD').rate
+    return currency
+  },
+}
+
+const card_api = {
+  async loadProduct(id) {
+    const resp = await fetch(`${API_URL + id}`)
+    const json = await resp.json()
+    return json.payload
+  },
+
+  async loadCourse() {
+    const resp = await fetch(`${API_URL_COURSET}`)
     return await resp.json()
+  },
+
+  async loadSimilarProducts(id) {
+    let ID = `${id}`
+    const resp = await fetch(`${API_URL + ID + '/similar/'}`)
+
+    return (await resp.json()).payload
   },
 }
